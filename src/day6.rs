5811d6951ed3e5e0289dab08ce0743b3e49078b3
Valentin Geyer <trayshar@t-online.de>
2022-12-06 09:38:34 +0100

    Moved function to library

--- src/day6.rs
+++ src/day6.rs
@@ -1,11 +1,11 @@
 use aoc22::read_lines;
+use aoc22::contains_duplicates;
 
 fn main() {
     let line = read_lines("./res/day6.txt").expect("Couldn't read file!").next().expect("Couldn't read file!");
 
     for (index, bytes) in line.as_bytes().windows(4).enumerate() {
-        // Checks for duplicate values in the slice, see https://stackoverflow.com/a/46766782
-        if !(1..bytes.len()).any(|i| bytes[i..].contains(&bytes[i - 1])) {
+        if !contains_duplicates(bytes) {
             println!("Found start-of-packet marker at {}", index + 4);
             break;
         }
@@ -14,8 +14,7 @@ fn main() {
     // ################################## Part 2 #########################################
 
     for (index, bytes) in line.as_bytes().windows(14).enumerate() {
-        // Checks for duplicate values in the slice, see https://stackoverflow.com/a/46766782
-        if !(1..bytes.len()).any(|i| bytes[i..].contains(&bytes[i - 1])) {
+        if !contains_duplicates(bytes) {
             println!("Found start-of-message marker at {}", index + 14);
             break;
         }
--- src/lib.rs
+++ src/lib.rs
@@ -11,3 +11,9 @@ where P: AsRef<Path>, {
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines().map(|l| l.expect("Couldn't read line?!")))
 }
+
+/// Checks for duplicate values in a slice
+pub fn contains_duplicates<T: PartialEq>(slice: &[T]) -> bool {
+    // see https://stackoverflow.com/a/46766782
+    (1..slice.len()).any(|i| slice[i..].contains(&slice[i - 1]))
+}
