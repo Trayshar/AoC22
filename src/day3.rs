678f7ac9a08cbc018c44c2e92eb358daa4b84f38
Valentin Geyer <trayshar@t-online.de>
2022-12-06 17:09:15 +0100

    Merged loops

--- src/day3.rs
+++ src/day3.rs
@@ -12,8 +12,14 @@ fn char_to_value(c: char) -> u32 {
 }
 
 fn main() {
-    let mut sum: u32 = 0;
-    for line in read_lines("./res/day3.txt").expect("Couldn't read file!") {
+    // Part 1
+    let mut sum1: u32 = 0;
+
+    // Part2: Using 3 bitmasks with 64 bits. Each 3 iterations we AND them to get the one item they all have in common
+    let mut items: [u64; 3] = [0; 3];
+    let mut sum2: u32 = 0;
+
+    for (index, line) in read_lines("./res/day3.txt").expect("Couldn't read file!").enumerate() {
         let length = line.chars().count();
 
         // Constructing a bitmask where each set bit represents an item.
@@ -21,25 +27,19 @@ fn main() {
         let comp2 = line.chars().skip(length/2).map(char_to_value).fold(0u64, |acc, value| acc | (1 << value));
 
         // Using AND, we get the item both compartments share
-        sum += (comp1 & comp2).trailing_zeros();
-    }
+        sum1 += (comp1 & comp2).trailing_zeros();
 
-    println!("The sum of all incorrect items is {}", sum);
+        // ################################## Part 2 #########################################
 
-    // ################################## Part 2 #########################################
-    
-    // Using a bitmask with 64 bits. Each iteration we AND them to get the one item they all have in common
-    let mut items: [u64; 3] = [0; 3];
-    let mut sum: u32 = 0;
-    for (index, line) in read_lines("./res/day3.txt").expect("Couldn't read file!").enumerate() {
         let item = &mut items[index % 3];
         line.chars().map(char_to_value).for_each(|value| *item |= 1 << value);
 
         if index % 3 == 2 {
-            sum += items.iter().fold(u64::MAX, |acc, bits| acc & bits).trailing_zeros();
+            sum2 += items.iter().fold(u64::MAX, |acc, bits| acc & bits).trailing_zeros();
             items = [0; 3];
         }
     }
-    println!("The sum of all badges is {}", sum);
-    
+
+    println!("The sum of all incorrect items is {}", sum1);
+    println!("The sum of all badges is {}", sum2);
 }
